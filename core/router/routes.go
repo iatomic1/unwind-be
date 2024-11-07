5875b012ceaa8f8fa2b6f80f8416ef9dc11d641c
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-11-07 04:55:56 +0100

    feat: added functions for sending responses

--- core/router/routes.go
+++ core/router/routes.go
@@ -1,12 +1,7 @@
 package router
 
 import (
-	"net/http"
-	"path/filepath"
-
 	"github.com/adeyemialameen04/unwind-be/core/server"
-	"github.com/adeyemialameen04/unwind-be/internal/projectpath"
-	scalargo "github.com/bdpiprava/scalar-go"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,33 +10,7 @@ func SetupRouter(srv *server.Server) {
 	router.Use(gin.Logger(), gin.Recovery())
 
 	RegisterBookRoutes(srv, router.Group("/books"))
-
-	specUrl := filepath.Join(projectpath.Root, "/docs/swagger.json")
-
-	router.GET("/docs/", func(c *gin.Context) {
-		content, err := scalargo.NewV2(
-			scalargo.WithSpecURL("/docs/swagger.json"),
-			scalargo.WithMetaDataOpts(
-				scalargo.WithTitle("Unwind"),
-			),
-			scalargo.WithServers(scalargo.Server{
-				URL:         "http://localhost:8080",
-				Description: "Default Server",
-			}),
-			scalargo.WithTheme(scalargo.ThemeDeepSpace),
-		)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-
-		c.Header("Content-Type", "text/html; charset=utf-8")
-		c.String(http.StatusOK, content)
-	})
-
-	router.GET("/docs/swagger.json", func(c *gin.Context) {
-		c.File(specUrl)
-	})
+	RegisterDocsRoutes(router.Group("/docs"))
 
 	srv.Router = router
 }
