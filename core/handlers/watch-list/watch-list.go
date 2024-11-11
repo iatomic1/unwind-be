88c3a1a89d561fa5482b258f7f4d0705bab381e4
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-11-11 14:03:44 +0100

    feat: implemented adding to watch list

--- core/handlers/watch-list/watch-list.go
+++ core/handlers/watch-list/watch-list.go
@@ -8,6 +8,7 @@ import (
 	"github.com/adeyemialameen04/unwind-be/internal/db/repository"
 	"github.com/adeyemialameen04/unwind-be/internal/domain"
 	"github.com/gin-gonic/gin"
+	"github.com/golodash/galidator"
 )
 
 type Handler struct {
@@ -29,7 +30,7 @@ func NewWatchListHandler(srv *server.Server) *Handler {
 //	@Accept			json
 //	@Produce		json
 //	@Security		AccessTokenBearer
-//	@Success		200		{object}	server.SuccessResponse{data=repository.WatchList}
+//	@Success		200		{object}	server.SuccessResponse{data=[]repository.WatchList}
 //	@Failure		401		{object}	server.UnauthorizedResponse			"Unauthorized"
 //	@Failure		404		{object}	server.NotFoundResponse				"Profile not found"
 //	@Failure		500		{object}	server.InternalServerErrorResponse	"Internal server error"
@@ -66,4 +67,72 @@ func (h *Handler) GetWatchList(c *gin.Context) {
 	}
 
 	server.SendSuccess(c, watchList, server.WithMessage("WatchList retrieved successfully"))
+}
+
+// AddToList godoc
+//
+//	@Summary		Adds an anime to a user watch list
+//	@Description	Retrieves a user watch list.
+//	@Tags			User
+//	@Accept			json
+//	@Produce		json
+//	@Security		AccessTokenBearer
+//
+// @Param			Anime	body		repository.AddToListParams	true	"Anime Data"
+//
+//	@Success		200		{object}	server.SuccessResponse{data=repository.WatchList}
+//	@Failure		401		{object}	server.UnauthorizedResponse			"Unauthorized"
+//	@Failure		404		{object}	server.NotFoundResponse				""
+//	@Failure		500		{object}	server.InternalServerErrorResponse	"Internal server error"
+//	@Router			/user/watch-list [post]
+func (h *Handler) AddToList(c *gin.Context) {
+	var (
+		ctx = context.Background()
+		req repository.AddToListParams
+	)
+
+	if err := h.validateAddtoListRequest(c, &req); err != nil {
+		return
+	}
+
+	userId, err := domain.GetUserIDFromContext(c)
+	if err != nil {
+		return
+	}
+
+	tx, err := h.srv.DB.Begin(ctx)
+	if err != nil {
+		server.SendInternalServerError(c, err, server.WithMessage("Error beginning transaction"))
+		return
+	}
+	defer tx.Rollback(ctx)
+
+	req.UserID = userId
+	repo := repository.New(tx)
+	watchList, err := repo.AddToList(ctx, req)
+	if err != nil {
+		server.SendInternalServerError(c, err, server.WithMessage("Fail to Add to watch list"))
+		return
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		server.SendInternalServerError(c, err, server.WithMessage("Failed to commit transaction"))
+		return
+	}
+
+	server.SendSuccess(c, watchList, server.WithMessage("Added to watchList successfully"))
+}
+
+func (h *Handler) validateAddtoListRequest(c *gin.Context, req *repository.AddToListParams) error {
+	g := galidator.New().CustomMessages(galidator.Messages{
+		"required": "$field is required",
+		"min":      `$field can't be less than {min}`,
+	})
+	customizer := g.Validator(repository.AddToListParams{})
+
+	if err := c.ShouldBindJSON(req); err != nil {
+		server.SendValidationError(c, customizer.DecryptErrors(err))
+		return err
+	}
+	return nil
 }
